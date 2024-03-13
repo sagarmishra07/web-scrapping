df7f785654aa5c424c2e7b23743c6c740e2accb5
Sagar Mishra <sagar@ktmbees.com>
2024-03-13 15:13:25 +0545

    [Fixes]:pymupdf pdf reader

--- gemini.py
+++ gemini.py
@@ -2,6 +2,8 @@ import fitz
 import getpass
 import json
 import os
+from dotenv import load_dotenv
+
 from langchain.output_parsers import PydanticOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.prompts import PromptTemplate
@@ -10,6 +12,8 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field, validator
 from langchain_google_genai import GoogleGenerativeAI
+
+load_dotenv()
 
 
 class Document(BaseModel):
@@ -22,31 +26,39 @@ class Document(BaseModel):
     remark: str = Field(description="Remark")
 
 
-# api_key = getpass()
-#
-if "GOOGLE_API_KEY" not in os.environ:
-    os.environ["GOOGLE_API_KEY"] = getpass.getpass("AIzaSyCL6P36tSFVLCcEN5q_U-psAkiTmQODHJs")
+def load_pdf():
+    doc = fitz.open('quiz.pdf')
+    pages = ""
+    for page in doc:
+        pages += page.get_text()
+    return pages
 
-# # load pdf
 
-pdf_file = 'quiz.pdf'
+def initialize_gemini_model():
+    if "GOOGLE_API_KEY" not in os.environ:
+        os.environ["GOOGLE_API_KEY"] = getpass.getpass("AIzaSyCL6P36tSFVLCcEN5q_U-psAkiTmQODHJs")
 
-loader = PyPDFLoader(pdf_file)
+    pages = load_pdf()
+    parser = JsonOutputParser(pydantic_object=Document)
+    prompt = PromptTemplate(
+        template="Extract the information as specified but it will be an array of object return all data from pages and donot repeat if question already exists.{format_instructions}\n{context}\n",
+        input_variables=["context"],
+        partial_variables={"format_instructions": parser.get_format_instructions()},
+    )
+    llm = GoogleGenerativeAI(model="gemini-pro", temperature="0", convert_system_message_to_human=True)
 
-pages = loader.load()
+    chain = prompt | llm | parser
+    response = chain.invoke({
+        "context": pages
+    })
+    converted_response = json.dumps(response)
+    return converted_response
 
-parser = JsonOutputParser(pydantic_object=Document)
-prompt = PromptTemplate(
-    template="Extract the information as specified but it will be and array of object and object is seperated after 7 line in pdf return all data from pages.\n{format_instructions}\n{context}\n",
-    input_variables=["context"],
-    partial_variables={"format_instructions": parser.get_format_instructions()},
-)
-llm = GoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
 
-chain = prompt | llm | parser
-response = chain.invoke({
-    "context": pages
-})
-with open("gemini_output.json", "w") as write_file:
-    converted_response = json.loads(response)
-    json.dump(converted_response, write_file)
+def main_function():
+    response = initialize_gemini_model()
+    print(response)
+
+
+if __name__ == "__main__":
+    main_function()
